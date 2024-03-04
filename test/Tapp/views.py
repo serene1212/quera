3bb65ddff468c4c4c726437d6980dd439779ea49
arshia <arshiarezagholi1212@gmail.com>
2024-03-04 13:11:14 +0330

    view with query params

--- test/Tapp/views.py
+++ test/Tapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def signup_view(request,name,age,last_name):
-    return HttpResponse(f'Signup Completed! mr {name} {last_name} age= {age}')
+    x=request.GET.get('your id')
+    return HttpResponse(f'Signup Completed! mr {name} {last_name} age= {age}' your id is {x})
